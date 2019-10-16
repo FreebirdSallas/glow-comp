a9cf9bd0dbfa8ebc85af4aa33de38d0b89341c92
Cohen <dcoco91@gmail.com>
2019-10-15 21:08:52 -0400

    idk

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import logo from './logo.svg'
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { Route, Switch } from 'react-router-dom';
 import About from './components/About'
 
@@ -30,26 +31,37 @@ class App extends React.Component {
 
   render(){
     return(
-        <div className="App">
-          <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <p>
-              Edit <code>src/App.js</code> and save to reload.
-            </p>
-            <a
-              className="App-link"
-              href="https://reactjs.org"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn React
-            </a>
-          </header>
-          <p>{this.state.data}</p>
-        </div>
+      <Router>
+
+        <Switch>
+          <Route exact path="/" component={About} />
+        </Switch>
+
+      </Router>
+
+
+        // <div className="App">
+        //   <header className="App-header">
+        //     <img src={logo} className="App-logo" alt="logo" />
+        //     <p>
+        //       Edit <code>src/App.js</code> and save to reload.
+        //     </p>
+        //     <a
+        //       className="App-link"
+        //       href="https://reactjs.org"
+        //       target="_blank"
+        //       rel="noopener noreferrer"
+        //     >
+        //       Learn React
+        //     </a>
+        //   </header>
+        //   <p>{this.state.data}</p>
+        // </div>
 
     )
   }
 
 
   }
+
+  export default App;

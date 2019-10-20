c69cfad029c9ca5562739d0d7a2b3470dbd71c4b
Cohen <dcoco91@gmail.com>
2019-10-20 17:13:53 -0400

    added email validation to user model

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,16 +1,33 @@
 import React, {Component} from 'react';
 import {MDBBtn, MDBView, MDBMask, MDBContainer} from 'mdbreact';
 import EventTable from '../comps/EventTable';
+import API from '../utils/API';
 
 class ProfilePage extends Component {
   constructor (props) {
     super (props);
     this.state = {
-     
+      rows: [],
+      isLoggedIn: false,
     };
   }
 
- 
+  componentDidMount () {
+    API.isLoggedIn ().then (response => {
+      console.log (response);
+
+      if (response.data.user) {
+        API.getUser (response.data.user._id).then (response => {
+          console.log (response.data._doc);
+          this.setState ({
+            rows: response.data._doc.events,
+          });
+        });
+      } else {
+        //something that removes table and tells them to log in
+      }
+    });
+  }
 
   render () {
     return (
--- database/models/user.js
+++ database/models/user.js
@@ -2,10 +2,20 @@ const mongoose = require ('mongoose');
 const bcrypt = require ('bcrypt');
 const Schema = mongoose.Schema;
 
+const validateEmail = email => {
+  var re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  return re.test (email);
+};
+
 const userSchema = new Schema ({
   email: {
     type: String,
-    required: true,
+    required: 'Email address is required',
+    validate: [validateEmail, 'Please fill a valid email address'],
+    match: [
+      /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+      'Please fill a valid email address',
+    ],
   },
   password: {
     type: String,

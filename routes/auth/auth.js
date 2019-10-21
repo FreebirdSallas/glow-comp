ae46107f3a8de644851995d2efb1000f1f9dd7f1
Cohen <dcoco91@gmail.com>
2019-10-20 20:21:03 -0400

    logout route

--- routes/auth/auth.js
+++ routes/auth/auth.js
@@ -1,27 +1,36 @@
-const router = require('express').Router();
-const passport = require('../../passport');
+const router = require ('express').Router ();
+const passport = require ('../../passport');
 
 // matches auth/user
-router.route('/').get((req, res, next) => {
-    console.log('user');
-    console.log(req.user);
-    if(req.user){
-        res.json({ user: req.user })
-    } else {
-        res.json({ user: null });
-    }
-})
+router.route ('/').get ((req, res, next) => {
+  if (req.user) {
+    console.log ('found user, logging in');
+    res.json ({user: req.user});
+  } else {
+    res.json ({user: null});
+  }
+});
 
 // match POST /auth/user/login
-router.route('/login').post((req, res, next) => {
-    console.log(`login, req.body: ${req.body}`);
-    next();
-}, passport.authenticate('local'), (req, res) => {
-    console.log('logged in', req.user);
-    var userInfo = {
-        email: req.user.email
-    };
-    res.send(userInfo);
+router.route ('/login').post ((req, res, next) => {
+  console.log (`login, req.body: ${req.body}`);
+  next ();
+}, passport.authenticate ('local'), (req, res) => {
+  console.log ('logged in', req.user);
+  var userInfo = {
+    email: req.user.email,
+  };
+  res.send (userInfo);
+});
+
+//match post /auth/user/logout
+router.route ('/logout').post ((req, res, next) => {
+  if (req.user) {
+    req.logout ();
+    res.send ({msg: 'logging out'});
+  } else {
+    res.send ({msg: 'no user to logout'});
+  }
 });
 
 module.exports = router;
